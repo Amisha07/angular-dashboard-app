24054215291e196048649e20fe0de3a322b61f5a
Amisha07 <amishagadhia1707@gmail.com>
2020-05-26 23:54:46 -0500

    6 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+// import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    NgbModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
